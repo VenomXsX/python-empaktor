471c7ea97850c5e7262f8ca0473a13f4b2d75e24
Phan Nguyen Khai <phan.nkhai05@gmail.com>
2023-10-26 10:13:30 +0200

    Changes: - Made BWT to work with a secret key - Adapted lib.py and utils.py to work with new BWT

--- empaktor/cmp_burrows/burrows_wheeler.py
+++ empaktor/cmp_burrows/burrows_wheeler.py
@@ -1,24 +1,23 @@
 def transform_bwt(data: str) -> str:
     data = data.strip()
-    # EOF character
-    data += "$"
     table = []
     for i in range(len(data)):
         data = data[-1] + data[:-1]
         table.append(data)
     table = sorted(table)
+    key = table.index(data)
     output = ""
     for i in range(len(table)):
         output += table[i][-1]
-    return output
+    return output, key
 
 
-def inverse_bwt(transformed_data: str) -> str:
+def inverse_bwt(transformed_data: str, key: int) -> str:
     table = [""] * len(transformed_data)
     # add column
-    for i in range(len(transformed_data)):
+    for _ in range(len(transformed_data)):
         table = [transformed_data[i] + table[i]
                  for i in range(len(transformed_data))]
         table = sorted(table)
-    output = [row for row in table if row.endswith("$")][0].rstrip("$")
+    output = table[key]
     return output
